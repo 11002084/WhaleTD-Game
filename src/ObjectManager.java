6f923e66b3079cdc5851477f3b6de4220ca7dfce
11002084 <36611325+11002084@users.noreply.github.com>
2020-04-12 16:58:32 -0700

    4/12/20
    Working on a menu for the start of the game.

--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -14,7 +14,9 @@ import javax.swing.Timer;
 //I'd like to speak to your manager
 
 public class ObjectManager implements MouseListener, ActionListener {
-	//Enemy Spawning Variables
+	boolean gameStarted = false;
+
+	// Enemy Spawning Variables
 	int spawnStartTowel = 5000;
 	int spawnStartEraser = 30000;
 	int spawnStartSpray = 60000;
@@ -33,13 +35,13 @@ public class ObjectManager implements MouseListener, ActionListener {
 	int minSpawnDelayEraser = 200;
 	int minSpawnDelaySpray = 500;
 	int minSpawnDelayTrashcan = 1000;
-	
+
 	int spawnCounter = 0;
 	int spawnDelayReductionDelay = 20000;
 	int totalTime = 0;
 	Timer enemyTimer = new Timer(10, this);
-	
-	//Other Enemy Variables
+
+	// Other Enemy Variables
 	int towelMoney = 10;
 	int eraserMoney = 25;
 	int sprayMoney = 50;
@@ -56,14 +58,14 @@ public class ObjectManager implements MouseListener, ActionListener {
 	double trashcanSpeed = 1;
 	double theAnnihilatorSpeed = 1;
 	int reduceAmount;
-	
-	//Player Values
-	static int numLives = 1000000;
+
+	// Player Values
+	static int numLives = 10;
 	static int money = 100;
 	static int livesReduction = 1;
 	Timer moneyTimer = new Timer(1000, this);
-	
-	//Objects and Rectangles
+
+	// Objects and Rectangles
 	public static Enemy fake;
 	public static Rectangle arrowButton;
 	public static Rectangle rifleButton;
@@ -72,6 +74,7 @@ public class ObjectManager implements MouseListener, ActionListener {
 	public static Rectangle upgradeButton;
 	public static Rectangle exitButton2;
 	public static Rectangle instructionButton;
+	public static Rectangle startButton;
 
 	// Arraylist of Path Objects
 	public static ArrayList<Path> pathList = new ArrayList<Path>();
@@ -79,8 +82,12 @@ public class ObjectManager implements MouseListener, ActionListener {
 	public static ArrayList<Enemy> enemyList = new ArrayList<Enemy>();
 	public static ArrayList<Projectile> projectileList = new ArrayList<Projectile>();
 
-	//ObjectManager Constructor
+	// ObjectManager Constructor
 	ObjectManager() {
+		while (gameStarted == false) {
+			startButton = new Rectangle(690, 590, 95, 50);
+			instructionButton = new Rectangle(startButton.x, startButton.y + 60, 95, 50);
+		}
 		instructionButton = new Rectangle(1000, 10, 100, 100);
 		initTowers();
 		initPaths();
@@ -88,15 +95,14 @@ public class ObjectManager implements MouseListener, ActionListener {
 		fake = new Enemy(-69, -420, 0, 0, "eraser", 100, 2);
 		enemyTimer.start();
 		moneyTimer.start();
-
 	}
 
-	//Calculate Distance using Maths
+	// Calculate Distance using Maths
 	public static double calcDist(double x, double x2, double y, double y2) {
 		return Math.sqrt(((x - x2) * (x - x2)) + ((y - y2) * (y - y2)));
 	}
 
-	//Calculate Distances and Get Closest Enemy
+	// Calculate Distances and Get Closest Enemy
 	public static Enemy getClosestEnemy(double d, double e, double max) {
 		double shortestDistance = 10000;
 		int enemyIndex = -1;
@@ -199,17 +205,17 @@ public class ObjectManager implements MouseListener, ActionListener {
 		pathList.add(new Path(1360, -90, 100, 100));
 	}
 
-	//Initialize Enemies
+	// Initialize Enemies
 	void initEnemies() {
 		enemyList.add(new Enemy(-40, 185, 50, 50, "towel", 0, 1));
 	}
 
-	//Initialize Projectiles
+	// Initialize Projectiles
 	public static void addProjectile(Projectile projectile) {
 		projectileList.add(projectile);
 	}
 
-	//Add Enemy Object
+	// Add Enemy Object
 	public static void addEnemy(Enemy enemy) {
 		enemyList.add(enemy);
 	}
@@ -232,14 +238,14 @@ public class ObjectManager implements MouseListener, ActionListener {
 		purgeObjects();
 	}
 
-	//Check Lives and Exit Game if Lives is 0
+	// Check Lives and Exit Game if Lives is 0
 	void checkLives() {
 		if (numLives <= 0) {
-			//System.exit(0);
+			// System.exit(0);
 		}
 	}
 
-	//Check if Projectile Is Touching Enemy
+	// Check if Projectile Is Touching Enemy
 	void checkCollision() {
 		for (int i = 0; i < enemyList.size(); i++) {
 			for (int j = 0; j < projectileList.size(); j++) {
@@ -251,7 +257,7 @@ public class ObjectManager implements MouseListener, ActionListener {
 		}
 	}
 
-	//Check the Health of Enemies
+	// Check the Health of Enemies
 	void checkHealth() {
 		for (int i = 0; i < enemyList.size(); i++) {
 			if (enemyList.get(i).health <= 0) {
@@ -260,7 +266,7 @@ public class ObjectManager implements MouseListener, ActionListener {
 		}
 	}
 
-	//Remove Projectiles and Enemies that Aren't Alive
+	// Remove Projectiles and Enemies that Aren't Alive
 	void purgeObjects() {
 		for (int i = projectileList.size() - 1; i >= 0; i--) {
 			if (projectileList.get(i).isAlive == false) {
@@ -270,19 +276,19 @@ public class ObjectManager implements MouseListener, ActionListener {
 
 		for (int i = enemyList.size() - 1; i >= 0; i--) {
 			if (enemyList.get(i).isAlive == false) {
-				if(enemyList.get(i).type.equals("towel")) {
+				if (enemyList.get(i).type.equals("towel")) {
 					money += towelMoney;
 				}
-				
-				else if(enemyList.get(i).type.equals("eraser")) {
+
+				else if (enemyList.get(i).type.equals("eraser")) {
 					money += eraserMoney;
 				}
-				
-				else if(enemyList.get(i).type.equals("spray")) {
+
+				else if (enemyList.get(i).type.equals("spray")) {
 					money += sprayMoney;
 				}
-				
-				else if(enemyList.get(i).type.equals("trashcan")) {
+
+				else if (enemyList.get(i).type.equals("trashcan")) {
 					money += trashcanMoney;
 				}
 				enemyList.remove(i);
@@ -292,39 +298,54 @@ public class ObjectManager implements MouseListener, ActionListener {
 
 	// Draw Method
 	void draw(Graphics g) {
-		//Redrawing Background
-		g.setColor(Color.WHITE);
-		g.fillRect(0, 0, WhaleTD.WIDTH, WhaleTD.HEIGHT);
-		
-		for (int i = 0; i < pathList.size(); i++) {
-			pathList.get(i).draw(g);
-		}
-		for (int i = 0; i < enemyList.size(); i++) {
-			enemyList.get(i).draw(g);
-		}
-		for (int i = 0; i < towerList.size(); i++) {
-			towerList.get(i).drawTowerOutline(g);
-		}
-		for (int i = 0; i < towerList.size(); i++) {
-			towerList.get(i).draw(g);
-		}
-		for (int i = 0; i < towerList.size(); i++) {
-			towerList.get(i).drawMenu(g);
-		}
-		for (int i = 0; i < projectileList.size(); i++) {
-			projectileList.get(i).draw(g);
-		}
+		if (gameStarted == false) {
+			g.setColor(Color.WHITE);
+			g.fillRect(0, 0, WhaleTD.WIDTH, WhaleTD.HEIGHT);
 
-		g.setColor(Color.RED);
-		g.fillRect(instructionButton.x, instructionButton.y, instructionButton.width, instructionButton.height / 2);
-		g.setColor(Color.BLACK);
-		g.drawString("Click Me For", 1005, 25);
-		g.drawString("Instuctions", 1005, 50);
-		g.drawString("Money: $" + money, 650, 50);
-		g.drawString("Lives: " + numLives, 800, 50);
+			g.setColor(Color.RED);
+			g.fillRect(startButton.x, startButton.y, startButton.width, startButton.height);
+			g.fillRect(instructionButton.x, instructionButton.y, instructionButton.width, instructionButton.height);
+			g.setColor(Color.BLACK);
+			g.drawString("START GAME", startButton.x + 10, startButton.y + 30);
+			g.drawString("INSTRUCTIONS", instructionButton.x + 10, instructionButton.y + 30);
+			g.drawString("WHALE TOWER DEFENSE", startButton.x - 25, startButton.y - 20);
+			g.drawString("sorry the start menu is bad rn", 50, 50);
+
+		} else if (gameStarted == true) {
+			// Redrawing Background
+			g.setColor(Color.WHITE);
+			g.fillRect(0, 0, WhaleTD.WIDTH, WhaleTD.HEIGHT);
+
+			for (int i = 0; i < pathList.size(); i++) {
+				pathList.get(i).draw(g);
+			}
+			for (int i = 0; i < enemyList.size(); i++) {
+				enemyList.get(i).draw(g);
+			}
+			for (int i = 0; i < towerList.size(); i++) {
+				towerList.get(i).drawTowerOutline(g);
+			}
+			for (int i = 0; i < towerList.size(); i++) {
+				towerList.get(i).draw(g);
+			}
+			for (int i = 0; i < towerList.size(); i++) {
+				towerList.get(i).drawMenu(g);
+			}
+			for (int i = 0; i < projectileList.size(); i++) {
+				projectileList.get(i).draw(g);
+			}
+
+			g.setColor(Color.RED);
+			g.fillRect(instructionButton.x, instructionButton.y, instructionButton.width, instructionButton.height / 2);
+			g.setColor(Color.BLACK);
+			g.drawString("Click Me For", 1005, 25);
+			g.drawString("Instuctions", 1005, 50);
+			g.drawString("Money: $" + money, 650, 50);
+			g.drawString("Lives: " + numLives, 800, 50);
+		}
 	}
 
-	//Hide Tower Menus
+	// Hide Tower Menus
 	public static void disableMenus(Tower tower) {
 		for (int i = 0; i < towerList.size(); i++) {
 			if (towerList.get(i) != tower) {
@@ -333,16 +354,16 @@ public class ObjectManager implements MouseListener, ActionListener {
 		}
 	}
 
-	//Lose Life if Enemy Gets Past Gauntlet
+	// Lose Life if Enemy Gets Past Gauntlet
 	public static void loseLives(Enemy enemy) {
 		enemyList.remove(enemy);
 		numLives -= livesReduction;
 	}
-	
-	public static void theAnnihilatorProtocol()
-	{
-			enemyList.removeAll(enemyList);
+
+	public static void theAnnihilatorProtocol() {
+		enemyList.removeAll(enemyList);
 	}
+
 	@Override
 	public void mouseClicked(MouseEvent e) {
 
@@ -359,12 +380,12 @@ public class ObjectManager implements MouseListener, ActionListener {
 		// TODO Auto-generated method stub
 		for (int i = 0; i < towerList.size(); i++) {
 			if (towerList.get(i).isClicked(e.getX(), e.getY()) == true) {
-				arrowButton = new Rectangle((int)towerList.get(i).x, (int)towerList.get(i).y, 50, 50);
-				rifleButton = new Rectangle((int)towerList.get(i).x+50, (int)towerList.get(i).y, 50, 50);
-				cannonButton = new Rectangle((int)towerList.get(i).x, (int)towerList.get(i).y+50, 50, 50);
-				exitButton1 = new Rectangle((int)towerList.get(i).x+50, (int)towerList.get(i).y+50, 50, 50);
-				upgradeButton = new Rectangle((int)towerList.get(i).x, (int)towerList.get(i).y, 100, 50);
-				exitButton2 = new Rectangle((int)towerList.get(i).x, (int)towerList.get(i).y+50, 100, 50);
+				arrowButton = new Rectangle((int) towerList.get(i).x, (int) towerList.get(i).y, 50, 50);
+				rifleButton = new Rectangle((int) towerList.get(i).x + 50, (int) towerList.get(i).y, 50, 50);
+				cannonButton = new Rectangle((int) towerList.get(i).x, (int) towerList.get(i).y + 50, 50, 50);
+				exitButton1 = new Rectangle((int) towerList.get(i).x + 50, (int) towerList.get(i).y + 50, 50, 50);
+				upgradeButton = new Rectangle((int) towerList.get(i).x, (int) towerList.get(i).y, 100, 50);
+				exitButton2 = new Rectangle((int) towerList.get(i).x, (int) towerList.get(i).y + 50, 100, 50);
 			}
 		}
 
@@ -375,15 +396,19 @@ public class ObjectManager implements MouseListener, ActionListener {
 					+ "\n Green buttons build green towers, which shoot at a fast rate but deal low damage."
 					+ "\n Blue buttons build blue towers, which shoot at a medium rate and deal medium damage."
 					+ "\n Pink buttons build pink towers, which shoot at a slow rate but deal large damage."
-					+ "\n If you accidentally clicked an empty gray space, you can click the black button to close the build menu." + "\n"
-					+ "\n You need 40 gold to build a green tower."
-					+ "\n You need 60 gold to build a blue tower."
-					+ "\n You need 75 gold to build a pink tower." + "\n"
+					+ "\n If you accidentally clicked an empty gray space, you can click the black button to close the build menu."
+					+ "\n" + "\n You need 40 gold to build a green tower."
+					+ "\n You need 60 gold to build a blue tower." + "\n You need 75 gold to build a pink tower." + "\n"
 					+ "\n After you build a tower, you can also upgrade it using gold if you select it again."
 					+ "\n Upgrading towers makes them deal more damage to enemies."
 					+ "\n Upgrading a tower become more expensive as the level of the tower increases." + "\n"
 					+ "\n You gain gold passively over time, but also by killing enemies."
 					+ "\n Don't run out of lives or you lose and the screen will close.");
+		}
+
+		if (startButton.intersects(e.getX(), e.getY(), 1, 1)) {
+			gameStarted = true;
+			System.out.println("Game Start");
 		}
 	}
 
@@ -405,40 +430,40 @@ public class ObjectManager implements MouseListener, ActionListener {
 		if (e.getSource() == enemyTimer && spawnedTheAnnihilator == false) {
 			spawnCounter += 1;
 			totalTime += 10;
-			
+
 			if (totalTime > spawnStartTowel && spawnCounter % spawnDelayTowel == 0) {
 				enemyList.add(new Enemy(-40, 185, 50, 50, "towel", towelHealth, towelSpeed));
 			}
-			
+
 			if (totalTime > spawnStartEraser && spawnCounter % spawnDelayEraser == 0) {
 				enemyList.add(new Enemy(-40, 185, 50, 50, "eraser", eraserHealth, eraserSpeed));
 			}
-			
-			if(totalTime > spawnStartSpray && spawnCounter % spawnDelaySpray == 0) {
+
+			if (totalTime > spawnStartSpray && spawnCounter % spawnDelaySpray == 0) {
 				enemyList.add(new Enemy(-40, 185, 50, 50, "spray", sprayHealth, spraySpeed));
 			}
-			
-			if(totalTime > spawnStartTrashcan && spawnCounter % spawnDelayTrashcan == 0) {
+
+			if (totalTime > spawnStartTrashcan && spawnCounter % spawnDelayTrashcan == 0) {
 				enemyList.add(new Enemy(-40, 185, 50, 50, "trashcan", trashcanHealth, trashcanSpeed));
 			}
-			
-			if(totalTime > spawnStartTheAnnihilator && spawnedTheAnnihilator == false) {
+
+			if (totalTime > spawnStartTheAnnihilator && spawnedTheAnnihilator == false) {
 				theAnnihilatorProtocol();
 				enemyList.add(new Enemy(-40, 185, 50, 50, "annihilator", theAnnihilatorHealth, theAnnihilatorSpeed));
 				spawnedTheAnnihilator = true;
 			}
-			
+
 			if (totalTime % spawnDelayReductionDelay == 0) {
-				if(totalTime > spawnStartTowel && spawnDelayTowel > minSpawnDelayTowel) {
+				if (totalTime > spawnStartTowel && spawnDelayTowel > minSpawnDelayTowel) {
 					spawnDelayTowel -= spawnDelayReductionTowel;
 				}
-				if(totalTime > spawnStartEraser && spawnDelayEraser > minSpawnDelayEraser) {
+				if (totalTime > spawnStartEraser && spawnDelayEraser > minSpawnDelayEraser) {
 					spawnDelayEraser -= spawnDelayReductionEraser;
 				}
-				if(totalTime > spawnStartSpray && spawnDelaySpray > minSpawnDelaySpray) {
+				if (totalTime > spawnStartSpray && spawnDelaySpray > minSpawnDelaySpray) {
 					spawnDelaySpray -= spawnDelayReductionSpray;
 				}
-				if(totalTime > spawnStartTrashcan && spawnDelayTrashcan > minSpawnDelayTrashcan) {
+				if (totalTime > spawnStartTrashcan && spawnDelayTrashcan > minSpawnDelayTrashcan) {
 					spawnDelayTrashcan -= spawnDelayReductionTrashcan;
 				}
 			}
